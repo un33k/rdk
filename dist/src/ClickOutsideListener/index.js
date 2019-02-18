8ea9ccdc3b5959583e23cc5b5847b477c1163a93
Austin McDaniel <amcdaniel2@gmail.com>
2019-02-18 15:14:51 -0600

    remove dist

--- dist/src/ClickOutsideListener/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-exports.__esModule = true;
-__export(require("./ClickOutsideListener"));
-//# sourceMappingURL=module.js.map
-
-//# sourceMappingURL={"version":3,"file":"module.js","sourceRoot":"","sources":["module.tsx"],"names":[],"mappings":";;;;;AAAA,4CAAuC"}
--- dist/src/Overlay/OverlayContext.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-exports.OverlayContext = react_1.createContext({
-    close: function () { return undefined; }
-});
-//# sourceMappingURL=module.js.map
-
-//# sourceMappingURL={"version":3,"file":"module.js","sourceRoot":"","sources":["module.tsx"],"names":[],"mappings":";;AAAA,+BAAsC;AASzB,QAAA,cAAc,GAAG,qBAAa,CAAsB;IAC/D,KAAK,EAAE,cAAM,OAAA,SAAS,EAAT,CAAS;CACvB,CAAC,CAAC"}
