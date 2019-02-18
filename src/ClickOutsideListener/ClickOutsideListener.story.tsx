45f7a2688b830cb3deda7b62fc0e67915822a18b
Stephanie Yang <steppy452@gmail.com>
2019-02-18 16:12:13 -0600

    add ClickOutsideListener story

--- src/ClickOutsideListener/ClickOutsideListener.story.tsx
+++ src/ClickOutsideListener/ClickOutsideListener.story.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { storiesOf } from '@storybook/react';
+import { ClickOutsideListener } from './ClickOutsideListener';
 import * as css from './ClickOutsideListener.story.module.scss';
 
+function ClickOutsideListenerDemo() {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <Fragment>
+      Outside
+      <div className={css.outside}>
+        <ClickOutsideListener
+          onClickOutside={() => setVisible(false)}
+          onEscape={() => setVisible(false)}
+        >
+          <div className={css.inside}>
+            <div>Inside</div>
+            <button onClick={() => setVisible(true)}>Click me</button>
+            {visible && <div className={css.clickedMsg}>Clicked</div>}
+          </div>
+        </ClickOutsideListener>
+      </div>
+    </Fragment>
+  );
+}
+
 storiesOf('Click Outside Listener', module)
-  .add('Simple', () => <div className={css.test}>test</div>);
+  .add('Simple', () => <ClickOutsideListenerDemo /> );
