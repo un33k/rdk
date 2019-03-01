498cc0930619528d4cf7cbccbdbfbb410faea05d
Austin McDaniel <amcdaniel2@gmail.com>
2019-03-01 15:10:25 -0600

    upgrade storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,26 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
+import centered from '@storybook/addon-centered/react';
+import { themes } from '@storybook/theming';
+import { withInfo } from '@storybook/addon-info';
 
+addParameters({
+  options: {
+    theme: {
+      ...themes.dark,
+      animation: false,
+      brand: 'rdk',
+      title: 'rdk',
+      image: '',
+      url: ''
+    }
+  },
+});
+
+addDecorator(centered);
+addDecorator(withInfo);
+
+// Grep src for .story file extensions
 const req = require.context('../src', true, /\.story\.tsx/);
-
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
+const loadStories = () => req.keys().forEach(filename => req(filename));
 
 configure(loadStories, module);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,59 +5,63 @@ const autoprefixer = require('autoprefixer');
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
 
-module.exports = (_baseConfig, _env, config) => {
-  config.module.rules.push(
-    {
-      test: /\.(ts|tsx)$/,
-      include: resolve(__dirname, '../src'),
-      loader: require.resolve('babel-loader')
-    },
-    {
-      test: sassRegex,
-      exclude: sassModuleRegex,
-      loaders: ['style-loader', 'css-loader', 'sass-loader'],
-      include: resolve(__dirname, '../')
-    },
-    {
-      test: sassModuleRegex,
-      include: resolve(__dirname, '../'),
-      loaders: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            importLoaders: 1,
-            modules: true,
-            localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
-          }
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            ident: 'postcss',
-            plugins: () => [
-              autoprefixer({
-                flexbox: 'no-2009'
-              }),
-              postcssFocusVisible()
-            ]
-          }
-        },
-        'sass-loader'
-      ]
-    }
-  );
-
-  config.node = {
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty'
-  };
-
-  config.resolve.modules.push(resolve(__dirname, '../src'));
-  config.resolve.extensions.push('.ts', '.tsx');
-
-  return config;
-};
+module.exports = async ({ config }) => ({
+  ...config,
+  module: {
+    ...config.module,
+    rules: [
+      ...config.module.rules,
+      {
+        test: /\.(ts|tsx)$/,
+        include: resolve(__dirname, '../src'),
+        use: [
+          require.resolve('babel-loader'),
+          require.resolve("react-docgen-typescript-loader")
+        ]
+      },
+      {
+        test: sassRegex,
+        exclude: sassModuleRegex,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        include: resolve(__dirname, '../')
+      },
+      {
+        test: sassModuleRegex,
+        include: resolve(__dirname, '../'),
+        loaders: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  flexbox: 'no-2009'
+                }),
+                postcssFocusVisible()
+              ]
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: config.plugins,
+  resolve: {
+    ...config.resolve,
+    modules: [
+      ...config.resolve.modules,
+      resolve(__dirname, '../src')
+    ],
+    extensions: [...config.resolve.extensions, '.ts', '.tsx']
+  },
+});
